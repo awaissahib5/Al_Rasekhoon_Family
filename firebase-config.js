de06aefa36ff460534d6c37041838347332a3e59
Muhammad Awais <108065053+awaissahib5@users.noreply.github.com>
2026-08-28 14:01:15 +0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,7 +4,7 @@
 // → Web app → SDK setup and configuration → "Config".
 // See README.md for the full step-by-step.
 // ============================================================
-export const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyDaW7jef2fAyK1BNdZM_-U7u2AI9J72xSw",
   authDomain: "al-rasekhoon.firebaseapp.com",
   projectId: "al-rasekhoon",
@@ -12,8 +12,4 @@ export const firebaseConfig = {
   messagingSenderId: "211405002213",
   appId: "1:211405002213:web:43b9e4cac34292bce8cb0c",
   measurementId: "G-4NQKLP89FC"
-};
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
